8ad205d98cfc058f59d822124f2960c22bd71a48
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-07-22 10:04:24 +0700

    Change to helper

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
+const GNBaseUrl = "https://newsapi.org/v1/";
+const Popular = "?source=google-news&sortBy=top&apiKey=";
+const ApiKey = "e65b548e891e430bbf3b1c5d911a1064";
+
+function buildUrl (url) {
+  return GNBaseUrl + url + Popular + ApiKey
+}
+
 const vm = new Vue({
   el: '#app',
   data: {
     articles: []
   },
   mounted() {
-    axios.get("https://newsapi.org/v1/articles?source=google-news&sortBy=top&apiKey=e65b548e891e430bbf3b1c5d911a1064")
-    .then(response => {this.articles = response.data.articles})
+    this.getPosts('articles');
+  },
+  methods: {
+    getPosts(section) {
+      let url = buildUrl(section);
+      axios.get(url).then((response) => {
+        this.articles = response.data.articles;
+      }).catch( error => { console.log(error); });
+    }
   }
 });
